c60133a21a444dfccbe649f2079b6b12ff8e2566
Mark Aquiapao <mark.aquiapao@student.csulb.edu>
2019-09-08 17:33:56 -0700

    Revised Comments

--- Assign1.cpp
+++ Assign1.cpp
@@ -1,41 +1,47 @@
 /*
  
+ ---------------------------------------------------------------------------
  CECS 326 Assignment 1: Programming Review
  Instructor: Ratana Ngo
- Assign1.cpp
- Assign1_ProgramReview file
- 
+ File: Assign1.cpp
+ Located in Assign1_ProgramReview folder
+ ---------------------------------------------------------------------------
  Created by Mark Aquiapao on 9/2/2019.
  Copyright © 2019 Mark Aquiapao. All rights reserved.
- 
- Rev A by Mark Aquiapao on 9/5/2019
+ ---------------------------------------------------------------------------
+ * Rev A by Mark Aquiapao on 9/5/2019
  Description: Redid initialization for library & recent list.
  Added function for finding keyword in document.
  
- Rev B by Mark Aquiapao on 9/7/2019
+ * Rev B by Mark Aquiapao on 9/7/2019
  Description: Revised function for searching a substring within a string.
  Added and revsied algrorithm for sorting out the library
  and recent list structures if a document in relecent list
  does not match.
  Added logic to verify entry in dictionary and revised display.
+ ---------------------------------------------------------------------------
+ Performance of Software on 9/8/2019:
+ - CPU: 71%-99%
+ - Memory: > 3 GB
+ - Energy Impact: High
+ ---------------------------------------------------------------------------
  
  */
 
-
+// Header Files
 #include <iostream>
 #include <string>
 #include <algorithm>
 #include <vector>
 using namespace std;
 
-// Prototypes:
+// Prototype:
 bool SearchWord(string, string);
-void SwapDocs(void);
 
 // Declare Constants and Globals
-
 const int   DOC_SIZE = 1024, POINTER = 128, MAX_MEM = 3000000, MIN_MEM = 2000000;
 
+// Structures for library and recent list
 struct library {
     string doc;
 };
@@ -44,6 +50,7 @@ struct recent_list {
     string rl_doc;
 };
 
+// Dictonary to verfiy user input
 string dictionary[15] = {   "FIRST",
     "CPP",
     "REVIEW",
@@ -104,7 +111,6 @@ int main()
     do
     {
         valid = false;
-        
         cout << "Dictionary: FIRST, CPP, REVIEW, PROGRAM, ASSIGNMENT, CECS, BEACH, ECS,\n";
         cout << "            FALL, SPRING, OS, MAC, LINUX, WINDOWS, LAB \n\n";
         cout << "Enter keyword from dictionary: ";
@@ -127,13 +133,14 @@ int main()
         count = 0;
         for(int pos=0; pos<POINTER; pos++)
         {
+            // For all documents that do NOT match keyword:
             if(SearchWord((ptr+pos)->rl_doc, key)==false)
-            { // For all documents that do NOT match keyword:
+            {
                 cout << "Document [" << count << "] ejected...Please wait...\n";
                 count++; // document found, increment count
                 
-                string lib_temp = lib_document[0].doc; // Save first document from library
-                int temp_memSize = rl_memSize[pos]; // Save size of ejected doc from recent list
+                string lib_temp = lib_document[0].doc;  // Save first document from library
+                int temp_memSize = rl_memSize[pos];     // Save size of ejected doc from recent list
                 
                 // Eject document from recent list and shift other documents
                 for(int i=pos; i<POINTER; i++)
@@ -147,7 +154,7 @@ int main()
                 // Shift documents in library up and leave last element empty/null
                 for(int j=0; j<DOC_SIZE; j++)
                 {
-                    if(j!=DOC_SIZE-1) // Shift the library documents up
+                    if(j!=DOC_SIZE-1) // Shift the library documents
                         lib_document[j].doc = lib_document[j+1].doc;
                     else
                         lib_document[j].doc = " "; // Empty value for last index
@@ -155,11 +162,11 @@ int main()
                 
                 
                 // Reinitialized last library document with same size as ejected document
-                // Insert swapped 1st document of library to end of recent list
                 for(int j=0; j<temp_memSize; j++)
                 {
                     lib_document[DOC_SIZE-1].doc += rand() % 25 + 'A'; // pick randomly from A-Z
                 }
+                // Insert swapped 1st document of library to end of recent list
                 (ptr+(POINTER-1))->rl_doc = lib_temp;
             }
         }
@@ -189,3 +196,5 @@ bool SearchWord(string document, string key)
         found = true; // key is found in document
     return found;
 }
+
+
